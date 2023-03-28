b84aa06c6c4ec9a5f4a3f0f634d3b65943f241ca
Drazen Dodik <drazend@live.fi>
2023-03-28 15:21:33 +0300

    Add ASG creation and update SGs

---
+++ Module/ASG/variables.tf
@@ -0,0 +1,52 @@
+variable "vpc_id" {
+  description = "VPC Id where the Valohai Postgresql database will be placed in."
+  type        = string
+}
+
+variable "subnet_ids" {
+  description = "A list of subnets where Valohai workers can be placed"
+  type        = list(string)
+}
+
+variable "ami" {
+  description = "AMI to be used for the Valohai Workers of this ASG"
+  type        = string
+}
+
+variable "redis_url" {
+  description = "Address of the redis (node) that will host the job queue and short term logs."
+  type        = string
+}
+
+variable "instance_type" {
+  description = "Instance type of Valohai workers"
+  type        = string
+}
+
+variable "security_group_id" {
+  description = "Security Groups to attach to this type"
+  type        = string
+}
+
+variable "region" {
+  description = "Region"
+  type        = string
+}
+
+variable "assign_public_ip" {
+  description = "Defines is workers should get a public IP"
+  type        = bool
+  default     = false
+}
+
+variable "instance_profile" {
+  description = "InstanceProfile to attach to Valohai Workers"
+  type        = string
+  default     = "ValohaiWorkerProfile"
+}
+
+variable "ebs_disk_size" {
+  description = "EBS disk size for Valohai instances"
+  type        = string
+  default     = "50"
+}
--- main.tf
+++ main.tf
@@ -1,27 +1,23 @@
 terraform {
-
   required_version = "1.4.2"
-
   required_providers {
     aws = {
       source = "hashicorp/aws"
-
     }
   }
 }
 
 provider "aws" {
   profile = var.aws_profile
-  region = var.region
+  region  = var.region
 }
-
 
 module "Database" {
   source = "./Module/Postgres"
 
   vpc_id             = var.vpc_id
   db_subnet_ids      = var.db_subnet_ids
-  security_group_ids = [module.SecurityGroups.roi_sg, module.SecurityGroups.workers_sg]
+  security_group_ids = [module.SecurityGroups.roi_sg.id, module.SecurityGroups.workers_sg.id]
 
 }
 
@@ -68,11 +64,30 @@ module "EC2" {
   ec2_key           = var.ec2_key
   region            = var.region
   subnet_id         = var.public_subnet_id
-  security_group_id = module.SecurityGroups.roi_sg
+  security_group_id = module.SecurityGroups.roi_sg.id
   environment_name  = var.environment_name
   db_url            = module.Database.database_url
   db_password       = module.Database.database_password
   redis_url         = module.Redis.redis_url
 
   depends_on = [module.Database, module.IAM_Master, module.Redis]
+}
+
+module "ASG" {
+  source = "./Module/ASG"
+
+  for_each = toset(var.aws_instances_types)
+
+  vpc_id            = var.vpc_id
+  subnet_ids        = var.worker_subnet_ids
+  security_group_id = module.SecurityGroups.workers_sg.id
+  instance_type     = each.key
+  region            = var.region
+  redis_url         = module.Redis.redis_url
+  assign_public_ip  = true
+  ami               = "" # Leave empty for default
+
+  depends_on = [
+    module.SecurityGroups, module.IAM_Workers
+  ]
 }
