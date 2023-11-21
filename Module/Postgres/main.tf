b478948a86bcde20e7b1f18df87eae028dad5157
Drazen Dodik <drazend@live.fi>
2023-11-21 08:10:43 +0200

    Enable RDS delete protection by default

--- Module/Postgres/main.tf
+++ Module/Postgres/main.tf
@@ -141,7 +141,7 @@ resource "aws_db_instance" "valohai_roidb" {
   backup_retention_period   = 3
   skip_final_snapshot       = true
   final_snapshot_identifier = "valohai-roidb-latest"
-  deletion_protection       = false
+  deletion_protection       = true
 
   depends_on = [
     aws_db_subnet_group.valohai_roidb_subnet
